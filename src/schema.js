838e889f05fc6f6aafb438e639bb7d7bfb3fb4da
Harrison Harnisch <hharnisc@gmail.com>
2017-03-14 11:16:10 -0500

    add mutations

--- src/schema.js
+++ src/schema.js
@@ -6,8 +6,17 @@ export const schema = buildSchema(`
   type Query {
     count: Int
   }
+
+  type Mutation {
+    increment: Int
+    decrement: Int
+    set(value: Int): Int
+  }
 `);
 
 export const root = {
   count: () => count,
+  increment: () => ++count,
+  decrement: () => --count,
+  set: ({ value }) => count = value,
 };
